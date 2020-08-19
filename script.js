d52c4a812b2949ddc56de251b274907ea970e78b
naomi <iam.naomi92@gmail.com>
2020-08-19 21:52:20 +0900

    challenge9

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+//画像情報のオブジェクトを格納した配列
+const picArray = [
+{src: './images/pic_1.jpg', title: 'pic1'},
+{src: './images/pic_2.jpg', title: 'pic2'},
+{src: './images/pic_3.jpg', title: 'pic3'},
+{src: './images/pic_4.jpg', title: 'pic4'},
+{src: './images/pic_5.jpg', title: 'pic5'},
+{src: './images/pic_6.jpg', title: 'pic6'}
+];
+
+
+//スライドショーが再生中かどうかを記録しておくためのグローバル変数
+let playingID = 0;
+
+//写真を切り替える関数
+//関数名「changePicture」
+function changePicture() {
+
+}
+
+// ボタンを押してスライドショーを再生・停止する関数
+//関数名「playSlidedeshow」
+function playSlideshow() {
+    
+}
