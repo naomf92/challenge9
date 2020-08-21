440d5e6282735be65a9cdb07d00eba5d2ee74fb6
naomi <iam.naomi92@gmail.com>
2020-08-21 22:28:43 +0900

    challenge9

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ const picArray = [
 {src: './images/pic_6.jpg', title: 'pic6'}
 ];
 
+
 //スライドショーが再生中かどうかを記録しておくためのグローバル変数
 let playingID = 0;
+
+for(let i = 0; i<=6; i++);
+// スライドをカウントさせる変数（カウンター変数とよく言います）を作りましょう
+// 配列は、中身に割り振られた番号（インデックス番号）を呼び出すことができますので、その性質を利用します
 
 //写真を切り替える関数
 //関数名「changePicture」
 function changePicture() {
+    // 上記で用意をしたカウンター変数と配列を条件式に利用します
+    // ヒント: 配列の中身の数を把握すると、画像が最後の枚数に達するまで、と表現ができます。値の中身の数を把握するメソッドに.lengthがあるので、使用すると良さそうです
+    
      if( == ) {
         document.getElementById('pics').src = './images/pic_1.jpg';
         document.getElementById('pic-title').innerHTML = picArray[0].title
