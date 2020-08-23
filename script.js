17c606a1580d48a4f1f679b6f13fd7c60eaf87d9
naomi <iam.naomi92@gmail.com>
2020-08-23 14:18:25 +0900

    challenge9

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 //画像情報のオブジェクトを格納した配列
 const picArray = [
-{src: './images/pic_1.jpg', title: 'pic1'},
-{src: './images/pic_2.jpg', title: 'pic2'},
-{src: './images/pic_3.jpg', title: 'pic3'},
-{src: './images/pic_4.jpg', title: 'pic4'},
-{src: './images/pic_5.jpg', title: 'pic5'},
-{src: './images/pic_6.jpg', title: 'pic6'}
+{src: './images/pic_1.jpg', title: 'picture1'},
+{src: './images/pic_2.jpg', title: 'picture2'},
+{src: './images/pic_3.jpg', title: 'picture3'},
+{src: './images/pic_4.jpg', title: 'picture4'},
+{src: './images/pic_5.jpg', title: 'picture5'},
+{src: './images/pic_6.jpg', title: 'picture6'}
 ];
 
 
 //スライドショーが再生中かどうかを記録しておくためのグローバル変数
 let playingID = 0;
 
-for(let i = 0; i<=6; i++)
+let counter = 0;
 // スライドをカウントさせる変数（カウンター変数とよく言います）を作りましょう
 // 配列は、中身に割り振られた番号（インデックス番号）を呼び出すことができますので、その性質を利用します
 
@@ -23,12 +23,18 @@ for(let i = 0; i<=6; i++)
 function changePicture() {
     // 上記で用意をしたカウンター変数と配列を条件式に利用します
     // ヒント: 配列の中身の数を把握すると、画像が最後の枚数に達するまで、と表現ができます。値の中身の数を把握するメソッドに.lengthがあるので、使用すると良さそうです
-    if(==) {
+    
+    for(let counter=0; counter<=picArray.length; counter++)
+    
+    if(counter == 1) {
         document.getElementById('pics').src = './images/pic_1.jpg';
-        document.getElementById('pic-title').innerHTML = picArray[0].title
-    }  
+        document.getElementById('pic-title').innerHTML = picArray[0].title  
+    } else if (counter == 2) {
+        document.getElementById('pics').src = './images/pic_2.jpg';
+        document.getElementById('pic-title').innerHTML = picArray[1].title
+    } 
 }
-setInterval(function() {changePicture();}, 2000);
+// setInterval(function() {changePicture();}, 2000);
 
 
 // ボタンを押してスライドショーを再生・停止する関数
